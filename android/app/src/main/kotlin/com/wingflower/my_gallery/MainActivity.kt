10d03234efff37fb1f60d4898d9df17e414d62bf
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-21 15:50:57 +0900

    23_12_21 init

---
+++ android/app/src/main/kotlin/com/wingflower/my_gallery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wingflower.my_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyGalleryApp(),
+    );
+  }
+}
+
+class MyGalleryApp extends StatefulWidget {
+  const MyGalleryApp({super.key});
+
+  @override
+  State<MyGalleryApp> createState() => _MyGalleryAppState();
+}
+
+class _MyGalleryAppState extends State<MyGalleryApp> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? images;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('전자액자'),
+      ),
+      body: images == null
+          ? const Center(child: Text('No data'))
+          : PageView(
+              children: images!.map((image) {
+                return FutureBuilder<Uint8List>(
+                    future: images![0].readAsBytes(),
+                    builder: (context, snapshot) {
+                      final data = snapshot.data;
+
+                      if (data == null ||
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      return Image.memory(
+                        data,
+                        width: double.infinity,
+                      );
+                    });
+              }).toList(),
+            ),
+    );
+  }
+}
